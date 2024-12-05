f4e0bd74b1967a5e1db313c818c823f9bdcea665
kaleab gemechu <kgemechu908@gmail.com>
2024-12-05 03:34:58 +0300

    built the header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,72 +1,69 @@
-import Link from "./Link";
+// import Link from "./Link";
 import PageNav from "./PageNav";
 import { motion } from "framer-motion";
 import { fadeIn } from "../variants";
-import Navbar from "./Navbar";
-import cube from "../assets/images/cube.png";
-import shape5 from "../assets/images/shape-05.webp";
-import shape3 from "../assets/images/shape-03.webp";
+import { MdOutlineArrowRightAlt } from "react-icons/md";
+import { FaVideo } from "react-icons/fa";
+
+// import Navbar from "./Navbar";
+// import cube from "../assets/images/cube.png";
 
 function Header() {
   return (
-    <div className="relative h-screen bg-gradient-to-b from-[#FBF7F4] to-[#fbf7f400] overflow-hidden">
+    <div className="relative h-screen bg-[#f1f1f1] overflow-hidden">
       {" "}
       <PageNav />
-      <Navbar />
-      <div className="flex justify-center">
-        <p className="capitalize text-center text-secondary text-[14px] font-medium py-2 px-[11px] rounded-[5px] bg-gradient-to-r from-[#FEE7C4] to-[#BEE5E4] mt-32 phone:mt-20 mtab:mt-24 tablet:mt-28 laptop:mt-32">
-          boost your productivity
-        </p>
+      {/* <Navbar /> */}
+      <div className="max-w-[1200px] m-auto flex flex-col gap-2">
+        <div className="flex">
+          <p className="bg-[#2289FF]/15 text-[#2289FF] text-[14px] font-medium py-2 px-[11px] rounded-[5px] mt-28">
+            Empowering Opportunity. Advancing Ethiopia.
+          </p>
+        </div>
+        <motion.h1
+          variants={fadeIn("left", 0.3)}
+          initial="hidden"
+          whileInView={"show"}
+          viewport={{ once: false, amount: 0.7 }}
+          className="font-semibold text-7xl w-[700px]"
+        >
+          Empowering Individuals, Businesses & Nations
+        </motion.h1>
+        <motion.p
+          variants={fadeIn("right", 0.3)}
+          initial="hidden"
+          whileInView={"show"}
+          viewport={{ once: false, amount: 0.7 }}
+          className="text-[18px] text-[#949fa9] w-[750px] phone:text-[18px] mdphone:text-[18px] ptab:text-[19px] mtab:text-[20px] tablet:[22px] text-secondary leading-[1.36] mt-4"
+        >
+          The Ethiopian Labor Market Information System empowers individuals,
+          businesses, and nations by connecting skilled Ethiopian workers with
+          domestic and global employment opportunities, fostering economic
+          growth and development.
+        </motion.p>
+        <motion.div
+          variants={fadeIn("up", 0.3)}
+          initial="hidden"
+          whileInView={"show"}
+          viewport={{ once: false, amount: 0.7 }}
+          className="relative z-[999] flex gap-4 mt-10 phone:mt-6 tablet:mt-6 laptop:mt-10"
+        >
+          <button className="flex items-center gap-2 bg-[#000080] opacity-[0.6]  px-8 py-2 rounded-lg">
+            <p className="text-white font-semibold">Register</p>
+            <MdOutlineArrowRightAlt color="white" size={25} />
+          </button>
+          <button className="flex items-center gap-2 bg-[#2289FF]/15 px-4 py-2 rounded-lg">
+            <FaVideo size={30} color="yellow" />
+
+            <p>National Job Portal ላይ እንዴት ሥራ ማግኘት እንደሚቻል ይመልከቱ።</p>
+          </button>
+        </motion.div>
       </div>
-      <motion.h1
-        variants={fadeIn("left", 0.3)}
-        initial="hidden"
-        whileInView={"show"}
-        viewport={{ once: false, amount: 0.7 }}
-        className="font-colasta text-center text-6xl phone:text-4xl mtab:text-4xl tablet:text-4xl btablet:text-5xl laptop:text-6xl font-bold text-primary mt-4"
-      >
-        Drive More Customers
-        <br />
-        Through Digital
-      </motion.h1>
-      <motion.p
-        variants={fadeIn("right", 0.3)}
-        initial="hidden"
-        whileInView={"show"}
-        viewport={{ once: false, amount: 0.7 }}
-        className="text-center text-[22px] phone:text-[18px] mdphone:text-[18px] ptab:text-[19px] mtab:text-[20px] tablet:[22px] text-secondary leading-[1.36] mt-4  px-4"
-      >
-        Specializing in custom software, we create innovative <br /> solutions
-        to streamline operations and boost productivity.
-      </motion.p>
-      <motion.div
-        variants={fadeIn("up", 0.3)}
-        initial="hidden"
-        whileInView={"show"}
-        viewport={{ once: false, amount: 0.7 }}
-        className="relative z-[999] flex justify-center items-center mt-10 phone:mt-6 tablet:mt-6 laptop:mt-10"
-      >
-        <Link variant="primary" size="lg" className="bg-btn-bg-main font-bold">
-          Apply now
-        </Link>
-        <Link variant="secondary" size="lg" className="ml-4">
-          Read more
-        </Link>
-      </motion.div>
-      <div className="absolute top-[70%] phone:top-[85%] mtab:top-[80%] tablet:top-[75%] laptop:top-[70%] end-[-24%] z-[-1]">
-        <img src={shape3} />
-      </div>
-      <div className="absolute top-[95%] end-[1%] z-[100] animate-wc-y-anim">
-        <img src={shape5} />
-      </div>
-      <div className="absolute bottom-[calc(80%+13px)] start-[25%] cube z-[1] phone:opacity-0 laptop:opacity-100 bigdesktop:opacity-0">
-        <img src={cube} />
-      </div>
-      <div className="absolute h-[10px] w-[10px] rounded-lg bg-[#00c99c] top-[40%] start-[5%] bubble-one z-[100]"></div>
-      <div className="absolute h-[30px] w-[30px] rounded-2xl bg-[#fca249] top-[65%] start-[8%] bubble-two z-[100] phone:opacity-0 tablet:opacity-100"></div>
-      <div className="absolute h-[15px] w-[15px] rounded-2xl bg-[#7d95fa] top-[95%] start-[20%] bubble-two z-[100]"></div>
-      <div className="absolute h-[30px] w-[30px] rounded-2xl bg-[#ff9398] top-[65%] end-[8%] bubble-two z-[100] phone:opacity-0 tablet:opacity-100"></div>
-      <div className="absolute h-[15px] w-[15px] rounded-2xl bg-[#d27dfa] top-[40%] end-[5%] bubble-two z-[100]"></div>
+      <img src="/pic-1.svg" className="z-[999999] w-[350px] absolute -top-[20px] right-[270px]" />
+      <img src="/pic-2.svg" className="z-[999999] w-[250px] absolute top-[120px] right-[10px]" />
+      <img src="/pic-3.svg" className="z-[999999] w-[350px] absolute bottom-0 right-[0px]" />
+      <img src="/pic-4.svg" className="z-[999999] w-[350px] absolute bottom-[60px] right-[350px]" />
+      
     </div>
   );
 }
--- src/components/PageNav.jsx
+++ src/components/PageNav.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable react/no-unescaped-entities */
 import { useEffect, useState } from "react";
 import Link from "./Link";
@@ -36,35 +37,19 @@ function PageNav() {
   return (
     <>
       <div
-        className={`fixed top-0 left-0 w-full z-[100000] flex justify-between px-12 phone:px-2 mdphone:px-4 ptab:px-6 mtab:px-8 tablet:px-10 laptop:px-12 pt-6 pb-2 content-center items-center from-[#FBF7F4] to-[#fbf7f400] bg-gradient-180 overflow-hidden ${
-          isSticky ? "backdrop-blur-md bg-[#ffffff61]" : "bg-[#FBF7F4]"
+        className={`fixed top-0 left-0 w-full z-[100000] flex justify-between px-12 phone:px-2 mdphone:px-4 ptab:px-6 mtab:px-8 tablet:px-10 laptop:px-12 pt-6 pb-2 content-center items-center overflow-hidden bg-[#f1f1f1] ${
+          isSticky ? "shadow-bottom-only" : ""
         }`}
       >
-        <div
-          onClick={() => handleIsOpen()}
-          className="absolute top-4 right-12 phone:right-[12%] ptab:right-[8%] laptop:invisible"
-        >
-          {isOpen ? (
-            <IoCloseOutline
-              size={40}
-              color="btn-bg-main"
-              className="z-[1000]"
-            />
-          ) : (
-            <MdMenu size={40} color="btn-bg-main" className="z-[1000]" />
-          )}
-        </div>
         <div className="">
-          <p className="font-medium text-primary text-lg font-bold capitalise phone:text-base mtab:text-lg">
-            Abyssinia software solution
-          </p>
+          <img src="logo.png" className="h-[70px] w-[250px]" />
         </div>
         <ul
-          className={`flex gap-8 justify-center items-center mr-14 visible ${
+          className={`flex gap-20 justify-center items-center visible ${
             isHidden ? "" : "hidden"
           }`}
         >
-          <li className="font-colasta group py-2 relative font-medium text-primary capitalize text-base text-base hover:cursor-pointer hover:text-btn-bg-main transition-all duration-200">
+          <li className="font-colasta group py-2 relative font-bold text-[#000080] capitalize text-base text-base hover:cursor-pointer hover:text-btn-bg-main transition-all duration-200">
             <NavLink
               NavLink
               to="/"
@@ -73,26 +58,8 @@ function PageNav() {
               Home
             </NavLink>
           </li>
-          <li
-            onMouseEnter={() => setShowPages(true)}
-            onMouseLeave={() => setShowPages(false)}
-            onClick={() => setShowPages(!showPages)}
-            className={`font-colasta group py-2 relative font-medium text-primary capitalize text-base text-base hover:cursor-pointer hover:text-btn-bg-main transition-hover duration-200`}
-          >
-            <a className="before:inline-block before:content-[''] before:absolute before:bottom-[15%] before:w-full before:h-[2px] before:bg-btn-bg-main before:scale-x-0 before:origin-center group-hover:before:scale-x-100 before:transition-all before:duration-[200ms]">
-              pages
-            </a>
-          </li>
-          <li className="font-colasta group py-2 relative font-medium text-primary capitalize text-base text-base hover:cursor-pointer hover:text-btn-bg-main transition-hover duration-200">
-            <NavLink
-              to="/project"
-              className="before:block before:content-[''] before:absolute before:bottom-[15%] before:w-full before:h-[2px] before:bg-btn-bg-main before:scale-x-0 before:origin-center group-hover:before:scale-x-100 before:transition-all before:duration-[200ms]"
-            >
-              {" "}
-              portifolio
-            </NavLink>
-          </li>
-          <li className="font-colasta group py-2 relative font-medium text-primary capitalize text-base text-base hover:cursor-pointer hover:text-btn-bg-main transition-hover duration-200">
+
+          <li className="font-colasta group py-2 relative font-bold text-gray-500 hover:text-[#000080] capitalize text-base text-base hover:cursor-pointer hover:text-btn-bg-main transition-hover duration-200">
             <NavLink
               to="/about"
               className="before:block before:content-[''] before:absolute before:bottom-[15%] before:w-full before:h-[2px] before:bg-btn-bg-main before:scale-x-0 before:origin-center group-hover:before:scale-x-100 before:transition-all before:duration-[200ms]"
@@ -100,29 +67,31 @@ function PageNav() {
               about
             </NavLink>
           </li>
-          <li className="font-colasta group py-2 relative font-medium text-primary capitalize text-base text-base hover:cursor-pointer hover:text-btn-bg-main transition-hover duration-200">
+          <li className="font-colasta group py-2 relative font-bold text-gray-500 hover:text-[#000080] capitalize text-base text-base hover:cursor-pointer hover:text-btn-bg-main transition-hover duration-200">
             <NavLink
-              to="/blog"
+              to="/about"
               className="before:block before:content-[''] before:absolute before:bottom-[15%] before:w-full before:h-[2px] before:bg-btn-bg-main before:scale-x-0 before:origin-center group-hover:before:scale-x-100 before:transition-all before:duration-[200ms]"
             >
-              blog
+              Ossc Locations
             </NavLink>
           </li>
-          <li className="font-colasta group py-2 relative font-medium text-primary capitalize text-base text-base hover:cursor-pointer hover:text-btn-bg-main transition-hover duration-200">
+
+          <li className="font-colasta group py-2 relative font-bold text-gray-500 hover:text-[#000080] capitalize text-base text-base hover:cursor-pointer hover:text-btn-bg-main transition-hover duration-200">
             <NavLink
-              to="/contact"
+              to="/about"
               className="before:block before:content-[''] before:absolute before:bottom-[15%] before:w-full before:h-[2px] before:bg-btn-bg-main before:scale-x-0 before:origin-center group-hover:before:scale-x-100 before:transition-all before:duration-[200ms]"
             >
-              contact
+              Ossc Lists
             </NavLink>
           </li>
         </ul>
-        <div className={`${isHidden ? "" : "hidden"}`}>
-          <NavLink to="/contact">
-            <Link variant="primary" size="sm">
-              let's talk!
-            </Link>
-          </NavLink>
+        <div className="flex gap-4 ml-8">
+          <button className="px-8 py-2 border-2 border-[#000080] text-[#000080] rounded-lg">
+            login
+          </button>
+          <button className="px-4 py-2 bg-[#000080]  opacity-[0.6] hover:opacity-[0.7] transition-200 text-white rounded-lg">
+            Download App
+          </button>
         </div>
       </div>
     </>
