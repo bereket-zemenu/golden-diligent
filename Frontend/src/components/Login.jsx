c28eea3b17eebe18d304922bcff89aff9398eef3
kaleab gemechu <kgemechu908@gmail.com>
2024-12-13 00:39:23 +0300

    updated the login page

--- Frontend/src/components/Login.jsx
+++ Frontend/src/components/Login.jsx
@@ -36,22 +36,32 @@ const Login = () => {
           </p>
           <form className="space-y-4 w-80">
             <div>
-              {/* <label htmlFor="phone" className="block text-gray-700 mb-2">
-                Phone Number
-              </label> */}
-              <div className="relative flex items-center border border-blue-500 rounded-sm">
-                <span className="absolute left-2 pr-3 text-gray-600">+251</span>
+              <div className="relative z-1">
+                {/* Input Field */}
                 <input
                   type="text"
                   id="phone"
-                  placeholder="PhoneNumber"
-                  className="pl-[60px] w-full py-1 px-3 focus:outline-none "
+                  placeholder=" "
+                  className="relative peer z-1 w-full py-2 pl-[50px] pr-3 border border-blue-500 rounded-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
                 />
+
+                {/* Floating Label */}
+                <label
+                  htmlFor="phone"
+                  className="absolute left-[50px] top-1/2 transform -translate-y-1/2 text-gray-500 text-sm transition-all peer-placeholder-shown:translate-y-[-50%] peer-placeholder-shown:text-gray-400 peer-focus:top-[-2px] peer-focus:bg-white peer-focus:px-1 peer-focus:left-[45px] peer-focus:text-blue-500 peer-focus:text-xs"
+                >
+                  Phone Number
+                </label>
+
+                {/* Country Code */}
+                <span className="absolute left-2 top-1/2 transform -translate-y-1/2 text-gray-600 text-sm">
+                  +251
+                </span>
               </div>
             </div>
-            <div className="relative">
-              {/* <IoEyeOffOutline className="absolute right-5 top-1/2 transform -translate-y-1/2" /> */}
 
+            <div className="relative z-1">
+              {/* Eye Icon */}
               {isOpen ? (
                 <HiEye
                   onClick={() => setIsOpen(!isOpen)}
@@ -63,13 +73,24 @@ const Login = () => {
                   className="absolute right-5 top-1/2 transform -translate-y-1/2 cursor-pointer"
                 />
               )}
+
+              {/* Input Field */}
               <input
-                type="password"
+                type={isOpen ? "text" : "password"}
                 id="password"
-                placeholder="Password"
-                className="w-full border border-blue-500 rounded-sm py-1 px-3 focus:outline-none"
+                placeholder=" "
+                className="relative peer z-1 w-full py-2 px-3 border border-blue-500 rounded-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
               />
+
+              {/* Floating Label */}
+              <label
+                htmlFor="password"
+                className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-500 text-sm transition-all peer-placeholder-shown:translate-y-[-50%] peer-placeholder-shown:text-gray-400 peer-focus:top-[-2px] peer-focus:bg-white peer-focus:px-1 peer-focus:left-[10px] peer-focus:text-blue-500 peer-focus:text-xs"
+              >
+                Password
+              </label>
             </div>
+
             <p className="text-right text-[10px] text-blue-800">
               Forgot Password?
             </p>
