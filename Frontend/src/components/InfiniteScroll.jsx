1ad6dd39473e9379ff4b51e52759044dd2e53768
kaleab gemechu <kgemechu908@gmail.com>
2024-12-18 17:58:19 +0300

    made infiniteScroll page fully responsive

--- Frontend/src/components/InfiniteScroll.jsx
+++ Frontend/src/components/InfiniteScroll.jsx
@@ -8,9 +8,12 @@ const InfiniteImageScroll = () => {
   const images = [slide1, slide2, slide4, slide5];
 
   return (
-    <div className="relative overflow-hidden relative w-full bg-white my-10 py-8">
-      <div className="z-[999] absolute top-8 left-0 h-[85%] w-[5%] bg-white opacity-[0.25]"></div>
-      <div className="z-[999] absolute top-8 right-0 h-[85%] w-[5%] bg-white opacity-[0.25]"></div>
+    <div className="relative overflow-hidden w-full bg-white my-10 py-4 phone:py-4 mdphone:py-6 tablet:py-8 laptop:py-10">
+      {/* Left and Right Gradient Overlay */}
+      <div className="z-[10] absolute top-4 left-0 h-[80%] w-[10%] phone:w-[15%] mdphone:w-[12%] tablet:w-[8%] bg-white opacity-50"></div>
+      <div className="z-[10] absolute top-4 right-0 h-[80%] w-[10%] phone:w-[15%] mdphone:w-[12%] tablet:w-[8%] bg-white opacity-50"></div>
+
+      {/* Scrolling Image Container */}
       <motion.div
         className="flex"
         animate={{ x: ["0%", "-100%"] }}
@@ -23,12 +26,19 @@ const InfiniteImageScroll = () => {
       >
         {/* Map over the images array and duplicate for seamless looping */}
         {[...images, ...images].map((src, index) => (
-          <div key={index} className="w-[312px] h-[361px] flex-shrink-0 mx-4">
+          <div
+            key={index}
+            className="flex-shrink-0 mx-2 phone:mx-1 mdphone:mx-2 tablet:mx-4"
+            style={{
+              width: "100%",
+              maxWidth: "312px", // Default
+            }}
+          >
             <img
               src={src}
               alt={`Slide ${index}`}
               loading="lazy"
-              className="object-cover h-full w-full"
+              className="object-cover w-full h-[180px] phone:h-[200px] mdphone:h-[250px] tablet:h-[300px] laptop:h-[361px]"
             />
           </div>
         ))}
