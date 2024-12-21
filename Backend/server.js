f54efdd77a11ab55f00769217ab26aea02512ea0
kal_olani <123155716+kalolani@users.noreply.github.com>
2024-12-21 11:36:08 +0300

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -21,11 +21,11 @@ app.use("/api/user", userRouter);
 
 // Serve static files from the React build directory
 const __dirname = path.resolve();  // This emulates __dirname in ES Modules
-app.use(express.static(path.join(__dirname, "frontend", "build")));  // Adjust based on where your frontend build files are
+app.use(express.static(path.join(__dirname, "Frontend", "build")));  // Adjust based on where your frontend build files are
 
 // Catch-all route to serve the React index.html file for any route
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));  // Adjust based on your file structure
+  res.sendFile(path.resolve(__dirname, "Frontend", "build", "index.html"));  // Adjust based on your file structure
 });
 
 // Start the server
