77e670650fc2a491ad3b3ca484349e76f0f11e26
kal_olani <123155716+kalolani@users.noreply.github.com>
2024-12-21 17:09:24 +0300

    Update userModel.js

--- Backend/models/userModel.js
+++ Backend/models/userModel.js
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 const UserSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
+    phone: { type: Number, required: true },
   password: { type: String, required: true },
   otp: {
     type: String,
